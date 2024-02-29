3d850a8a9b69622be87af9dded7fd56e5077b5d5
ijfranco0407 <ianjoydfranco0407@gmail.com>
2024-02-29 17:03:46 +0800

    1

--- css grid/main.js
+++ css grid/main.js
@@ -3,9 +3,15 @@ const num2Txt = document.getElementById("num2")
 const modTxt = document.getElementById("mod")
 const conTxt = document.getElementById("con")
 const sumTxt = document.getElementById("sum")
+const diffTxt = document.getElementById("diff")
+const prodTxt = document.getElementById("prod")
+const qoutTxt = document.getElementById("qout")
+let qout = 0
 let mod = 0
 let con = 0
 let sum = 0
+let diff = 0
+let prod = 0
 
 function modulo() {
     if(num1Txt.value == "" || num2Txt.value == "") {
@@ -20,17 +26,40 @@ function concatenates() {
     if(num1Txt.value == "" || num2Txt.value == "") {
         alert("Fill-out the Missing Context")
     } else {
-        con = parseInt(num1Txt.value) + "" + parseInt(num2Txt.value)
-        conTxt.innerHTML = con
+        conTxt.innerHTML = num1Txt.value + " " + num2Txt.value
     }
 }
 
-function sum() {
+function Sum() {
     if(num1Txt.value == "" || num2Txt.value == "") {
         alert("Fill-out the Missing Context")
     } else {
         sum = parseInt(num1Txt.value) + parseInt(num2Txt.value)
         sumTxt.innerHTML = sum
+    }
+}
+function difference() {
+    if(num1Txt.value == "" || num2Txt.value == "") {
+        alert("Fill-out the Missing Context")
+    } else {
+        diff = parseInt(num1Txt.value) - parseInt(num2Txt.value)
+        diffTxt.innerHTML = diff
+    }
+}
+function product() {
+    if(num1Txt.value == "" || num2Txt.value == "") {
+        alert("Fill-out the Missing Context")
+    } else {
+        prod = parseInt(num1Txt.value) * parseInt(num2Txt.value)
+        prodTxt.innerHTML = prod
+    }
+}
+function qoutient() {
+    if(num1Txt.value == "" || num2Txt.value == "") {
+        alert("Fill-out the Missing Context")
+    } else {
+        qout = parseInt(num1Txt.value) / parseInt(num2Txt.value)
+        qoutTxt.innerHTML = qout
     }
 }
 
@@ -41,6 +70,12 @@ function clearEntries() {
     modTxt.innerHTML = ""
     conTxt.innerHTML= ""
     sumTxt.innerHTML= ""
+    diffTxt.innerHTML= ""
+    prodTxt.innerHTML= ""
+    qoutTxt.innerHTML= ""
+    qout
+    prod = 0
+    diff = 0
     con = 0
     sum = 0
     mod = 0
